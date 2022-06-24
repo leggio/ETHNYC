4edfdb5380ddd005ca7c3e58eed4c87f13758df7
leggio <harrison@legg.io>
2022-06-24 19:32:03 -0400

    first commit

---
+++ contracts/Royalty.sol
@@ -0,0 +1,280 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import 'hardhat/console.sol';
+
+
+contract Royalty is ERC721URIStorage, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _softwareIds;
+  Counters.Counter private _topLevels;
+
+  struct slice {
+    uint256 _len;
+    uint256 _ptr;
+  }
+
+  uint256[] allSoftware;
+  uint256[] allEvents;
+
+  uint256 softwareCount;
+
+  address _daoAddress;
+
+  struct Software {
+    string id;
+    uint256 parent;
+    uint256 level;
+    uint256 children;
+    uint256 price;
+  }
+
+  mapping(uint256 => Software) software;
+
+  constructor() ERC721("Royalty", "RYLTY") {
+    softwareCount = 0;
+  }
+
+  function getAllSoftware() public view returns (uint256) {
+    return softwareCount;
+  }
+
+  // TODO: maybe add a view function to return all top level software? --> function getTopLevelSoftware()
+  // probably better than iterating through every id and checking the level to see if
+  // it's a parent software...
+
+  function getChildren(string memory targetId)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    console.log("getting children for %s", targetId);
+    bool[] memory ids = new bool[](allSoftware.length);
+    uint256 trueCount = 0;
+    for (uint256 i = 0; i < allSoftware.length; i++) {
+        string memory id = software[allSoftware[i]].id;
+        if (startsWith(toSlice(id), toSlice(targetId))) {
+            ids[i] = true;
+            trueCount++;
+        } else {
+            ids[i] = false;
+        }
+    }
+
+    uint256[] memory childrenIds = new uint256[](trueCount);
+    uint256 trackTrue = 0;
+    for (uint256 i = 0; i < ids.length; i++) {
+        if (ids[i] == true) {
+            childrenIds[trackTrue] = i;
+            trackTrue++;
+        }
+    }
+
+    return childrenIds;
+  }
+
+  function getSoftwareDetails(uint256 id)
+    public
+    view
+    returns (
+      address,
+      string memory,
+      uint256,
+      uint256,
+      uint256,
+      uint256,
+      string memory
+    )
+  {
+    Software memory targetSoftware = software[id];
+    string memory uri = ERC721URIStorage.tokenURI(id);
+    address owner = ERC721.ownerOf(id);
+
+    return (
+      owner,
+      Strings.toString(id),
+      targetSoftware.parent,
+      targetSoftware.level,
+      targetSoftware.children,
+      targetSoftware.price,
+      uri
+    );
+  }
+
+  function mintTopLevelSoftware(address recipient, string memory tokenURI)
+    public
+    payable
+    returns (uint256)
+  {
+    Software memory newSoftware = Software(
+      Strings.toString(_topLevels.current()),
+      0,
+      1,
+      0,
+      0
+    );
+
+    console.log('Minting new parent NFT');
+
+    software[softwareCount] = newSoftware;
+    softwareCount += 1;
+
+    uint256 newItemId = _softwareIds.current();
+    _mint(recipient, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+
+    _softwareIds.increment();
+    _topLevels.increment();
+
+    allSoftware.push(newItemId);
+
+    return newItemId;
+  }
+
+  function mintSoftware(address recipient, uint256 parent, uint256 price, string memory tokenURI)
+    public
+    payable
+    returns (uint256)
+  {
+    require(price > 0, "price must be > 0");
+
+    console.log('Minting new child NFT');
+
+    Software memory parentSoftware = software[parent];
+    string memory parentId = parentSoftware.id;
+
+    string memory children = Strings.toString(parentSoftware.children);
+
+    string memory newSoftwareId = string(bytes.concat(bytes(parentId), bytes(children)));
+
+    Software memory newSoftware = Software(
+      newSoftwareId,
+      parent,
+      softwareCount + 1,
+      0,
+      price
+    );
+    
+    software[softwareCount] = newSoftware;
+    uint256 newItemId = _softwareIds.current();
+    _mint(recipient, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+
+    parentSoftware.children += 1;
+    softwareCount += 1;
+
+    allSoftware.push(newItemId);
+    _softwareIds.increment();
+
+    return newItemId;
+  }
+
+  function purchase(uint256 id) public payable returns (uint256) {
+    Software memory forSaleSoftware = software[id];
+
+    require(
+      forSaleSoftware.price == msg.value,
+      "Insufficient price for software"
+    );
+
+    address owner = ERC721.ownerOf(id);
+
+    (bool success, ) = owner.call{value: msg.value}("");
+
+    // TODO: Don't send full msg.value to owner of software
+    // here is where we will call the distribute function to traverse
+    // up the parents and calculate how much is reserved for each parent
+    // then we send the remaining to the id given here
+
+    // distribute function thoughts:
+    // two ways to do this...
+    // 1: keep eth in contract and require distribute function to be called by a user for their software
+    // ^^^ this would run the calculation adhoc for the software and distribute to all other software in the tree (naive approach)
+    // 2: call distribute automagically at the time of sale and do the distributions
+    // ^^^ this would probably require us to emit distribute events (like a psuedo event struct... not a solidity event)
+    // so that we can display each of the distribute amounts to the parent software owner
+
+    require(success, "Transaction failed");
+
+    return id;
+  }
+
+  function distribute(uint256 id, uint256 price) public payable returns (uint256) {
+    // get NFT object
+    // get parent of NFT
+    // loop until no more parents
+    // store all parent ids
+    // get owner of each parent
+    // send % to parent
+
+    Software memory child = software[id];
+    console.log(child.parent);
+    uint256[] memory parents = new uint256[](child.level + 1);
+
+    parents[0] = child.parent;
+
+    for (uint256 i = 1; i <= child.level; i++) {
+      uint256 currentParrent = parents[i-1];
+      Software memory current = software[currentParrent];
+      console.log(current.parent);
+      parents[i] = current.parent;
+    }
+
+    return id;
+  }
+
+
+  /*
+    * @dev Returns a slice containing the entire string.
+    * @param self The string to make a slice from.
+    * @return A newly allocated slice containing the entire string.
+    */
+  function toSlice(string memory self) internal pure returns (slice memory) {
+      uint256 ptr;
+      assembly {
+          ptr := add(self, 0x20)
+      }
+      return slice(bytes(self).length, ptr);
+  }
+
+  /*
+    * @dev Returns true if `self` starts with `needle`.
+    * @param self The slice to operate on.
+    * @param needle The slice to search for.
+    * @return True if the slice starts with the provided text, false otherwise.
+    */
+  function startsWith(slice memory self, slice memory needle)
+      internal
+      pure
+      returns (bool)
+  {
+      if (self._len < needle._len) {
+          return false;
+      }
+
+      if (self._ptr == needle._ptr) {
+          return true;
+      }
+
+      bool equal;
+      assembly {
+          let length := mload(needle)
+          let selfptr := mload(add(self, 0x20))
+          let needleptr := mload(add(needle, 0x20))
+          equal := eq(
+              keccak256(selfptr, length),
+              keccak256(needleptr, length)
+          )
+      }
+      return equal;
+  }
+
+
+}
---
+++ test/RoyaltyTests.js
@@ -0,0 +1,89 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+
+/**
+ * Test info:
+ * run with `npx hardhat test`
+ * 
+ * Since this is called from a test and not on-chain, the responses can be a little wonky from functions
+ * that aren't `view` functions
+ * 
+ * In that case, you'll need to preface the function name with `callStatic` i.e:
+ * `await royalty.callStatic.mintSoftware(.........)`
+ * 
+ * Since we're using Hardhat, we can make use of hardhats console and `console.log() things inside of the smart contract`
+ * ^^^ very useful for debugging in tests
+ * 
+ */
+
+describe("test royalty", function () {
+  // it("should mint a top level NFT", async function () {
+  //   const [owner] = await ethers.getSigners();
+  //   const Royalty = await ethers.getContractFactory("Royalty");
+  //   const royalty = await Royalty.deploy();
+  //   await royalty.deployed();
+
+  //   const initialCount = await royalty.getAllSoftware();
+  //   expect(initialCount).to.equal(0);
+    
+  //   await royalty.mintTopLevelSoftware(owner.address, 'www.google.com');
+  //   const currentCount = await royalty.getAllSoftware();
+  //   expect(currentCount).to.equal(1);
+  // });
+  
+  // it("should mint a child NFT and get details", async function () {
+  //   const [owner] = await ethers.getSigners();
+  //   const Royalty = await ethers.getContractFactory("Royalty");
+  //   const royalty = await Royalty.deploy();
+  //   await royalty.deployed();
+
+  //   await royalty.mintTopLevelSoftware(owner.address, 'www.parent.com');
+  //   await royalty.mintSoftware(owner.address, 0, 100, 'www.child.com');
+
+  //   const details = await royalty.getSoftwareDetails(1);
+  //   expect(details[6]).to.equal('www.child.com')
+  // });
+  
+  // it("should get list of the children given a parent id", async function () {
+  //   const [owner] = await ethers.getSigners();
+  //   const Royalty = await ethers.getContractFactory("Royalty");
+  //   const royalty = await Royalty.deploy();
+  //   await royalty.deployed();
+
+  //   await royalty.mintTopLevelSoftware(owner.address, 'www.parent1.com');
+  //   await royalty.mintSoftware(owner.address, 0, 100, 'www.child1.com');
+  //   await royalty.mintSoftware(owner.address, 0, 200, 'www.child2.com');
+
+  //   const children = await royalty.getChildren('0');
+
+  //   expect(children[1].toString()).to.equal('1');
+  //   expect(children[2].toString()).to.equal('2');
+  // });
+
+  // it("should allow a user to purchase a child software", async function () {
+  //   const [owner] = await ethers.getSigners();
+  //   const Royalty = await ethers.getContractFactory("Royalty");
+  //   const royalty = await Royalty.deploy();
+  //   await royalty.deployed();
+
+  //   await royalty.mintTopLevelSoftware(owner.address, 'www.parent1.com');
+  //   await royalty.mintSoftware(owner.address, 0, 100, 'www.child1.com');
+
+  //   await royalty.purchase(1, { value: 100 });
+
+  // });
+  it("should get all parents for distribute function", async function () {
+    const [owner] = await ethers.getSigners();
+    const Royalty = await ethers.getContractFactory("Royalty");
+    const royalty = await Royalty.deploy();
+    await royalty.deployed();
+
+    await royalty.mintTopLevelSoftware(owner.address, 'www.parent1.com');
+    await royalty.mintSoftware(owner.address, 0, 100, 'www.child1.com');
+    await royalty.mintSoftware(owner.address, 1, 100, 'www.child2.com');
+
+    await royalty.distribute(2, 100);
+
+  });
+});
